a9aadd0f2cc439668ba94ef487515b1d8de9edc9
JJ Bastida <jj@athlon.studio>
2024-03-20 15:19:46 -0400

    add zoom blacklist

--- init.js
+++ init.js
@@ -1,4 +1,8 @@
-if (window.history.length <= 1 && !document.referrer) {
+const blacklistedSites = [
+  'zoom.us/j/'
+];
+
+if (!blacklistedSites.find((query) => window.location.href.includes(query)) && window.history.length <= 1 && !document.referrer) {
   fetch(chrome.runtime.getURL('/index.html')).then(r => r.text()).then(html => {
     document.body.insertAdjacentHTML('beforeend', html.replace('/styles/popup.css', chrome.runtime.getURL('/styles/popup.css')));
     
